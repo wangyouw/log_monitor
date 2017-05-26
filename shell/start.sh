934ab2eadb0a48ea1bc38377dbbee70b50e7217a
汪优文 <wangyw3@ifeng.com>
2017-05-26 14:49:22 +0800

    增加log目录

--- shell/start.sh
+++ shell/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup php /home/wwwroot/log_monitor/monitor.php weixin >> /home/wwwroot/log_monitor/monitor.log &  #监控汇总日志，写入队列
-nohup php /home/wwwroot/log_monitor/sendmail.php weixin >> /home/wwwroot/log_monitor/sendmail.log & #读取队列，发送邮件
+nohup php /home/wwwroot/log_monitor/monitor.php weixin >> /home/wwwroot/log_monitor/log/monitor.log &  #监控汇总日志，写入队列
+nohup php /home/wwwroot/log_monitor/sendmail.php weixin >> /home/wwwroot/log_monitor/log/sendmail.log & #读取队列，发送邮件
 
 * 3 * * * cat /dev/null > /tmp/logstash_all.log  #每天定时清空日志收集文件，避免文件过大
